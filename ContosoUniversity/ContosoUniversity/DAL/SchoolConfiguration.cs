81c211ee5bba0b3a04eac077d4beb29c3bd2947e
Joe Reid <jreid@vnet.net>
2014-07-16 00:49:57 -0500

    sql command interception

---
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolConfiguration : DbConfiguration
+    {
+        public SchoolConfiguration()
+        {
+            DbInterception.Add(new SchoolInterceptorLogging());
+        }
+    }
+}
