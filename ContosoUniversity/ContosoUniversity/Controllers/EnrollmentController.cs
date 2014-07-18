b7a36ce0ea7964c6884b6b1af209496ec20cd5f9
Joe Reid <jreid@vnet.net>
2014-07-18 17:52:45 -0500

    removed tons of commented methods

--- ContosoUniversity/ContosoUniversity/Controllers/EnrollmentController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/EnrollmentController.cs
@@ -11,55 +11,6 @@ namespace ContosoUniversity.Controllers
     {
         private SchoolContext db = new SchoolContext();
 
-        //// GET: Enrollment
-        //public async Task<ActionResult> Index()
-        //{
-        //    var enrollments = db.Enrollments.Include(e => e.Course).Include(e => e.Student);
-        //    return View(await enrollments.ToListAsync());
-        //}
-
-        //// GET: Enrollment/Details/5
-        //public async Task<ActionResult> Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Enrollment enrollment = await db.Enrollments.FindAsync(id);
-        //    if (enrollment == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(enrollment);
-        //}
-
-        //// GET: Enrollment/Create
-        //public ActionResult Create()
-        //{
-        //    ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title");
-        //    ViewBag.StudentID = new SelectList(db.Students, "ID", "LastName");
-        //    return View();
-        //}
-
-        //// POST: Enrollment/Create
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Create([Bind(Include = "EnrollmentID,CourseID,StudentID,Grade")] Enrollment enrollment)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Enrollments.Add(enrollment);
-        //        await db.SaveChangesAsync();
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID);
-        //    ViewBag.StudentID = new SelectList(db.Students, "ID", "LastName", enrollment.StudentID);
-        //    return View(enrollment);
-        //}
-
         // GET: Enrollment/Edit/5
         public async Task<ActionResult> Edit(int? id, int? instructorID)
         {
@@ -74,8 +25,6 @@ namespace ContosoUniversity.Controllers
             }
             ViewBag.theCourseID = enrollment.CourseID;
             ViewBag.theInstructorID = instructorID;
-            //ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID);
-            //ViewBag.StudentID = new SelectList(db.Students, "ID", "LastName", enrollment.StudentID);
             return View(enrollment);
         }
 
@@ -90,39 +39,11 @@ namespace ContosoUniversity.Controllers
             {
                 db.Entry(enrollment).State = EntityState.Modified;
                 await db.SaveChangesAsync();
-                //return RedirectToAction("../Instructor/Details/" + instructorID + "?courseID=" + courseID);
                 return RedirectToAction("Details", "Instructor", new { id = instructorID, courseID = enrollment.CourseID });
             }
-            //ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID);
-            //ViewBag.StudentID = new SelectList(db.Students, "ID", "LastName", enrollment.StudentID);
+;
             return View(enrollment);
         }
-
-        //// GET: Enrollment/Delete/5
-        //public async Task<ActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Enrollment enrollment = await db.Enrollments.FindAsync(id);
-        //    if (enrollment == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(enrollment);
-        //}
-
-        //// POST: Enrollment/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> DeleteConfirmed(int id)
-        //{
-        //    Enrollment enrollment = await db.Enrollments.FindAsync(id);
-        //    db.Enrollments.Remove(enrollment);
-        //    await db.SaveChangesAsync();
-        //    return RedirectToAction("Index");
-        //}
 
         protected override void Dispose(bool disposing)
         {
