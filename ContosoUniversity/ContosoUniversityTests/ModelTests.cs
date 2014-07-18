7ab9e6a67e6f94c66ad8e5353202fa4c5c379788
Joe Reid <jreid@vnet.net>
2014-07-18 01:51:38 -0500

    begining of model tests

---
+++ ContosoUniversity/ContosoUniversityTests/ModelTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversityTests
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void TestCourse()
+        {
+            Course c = new Course();
+            Assert.AreEqual(0, c.Enrollments.Count);
+        }
+    }
+}
