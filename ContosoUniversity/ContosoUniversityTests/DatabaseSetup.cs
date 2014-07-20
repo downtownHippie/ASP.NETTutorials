abae8aa73ab3339861d1387bfedad7777f6cc151
Joe Reid <jreid@vnet.net>
2014-07-20 18:36:05 -0500

    prep for more tests

--- ContosoUniversity/ContosoUniversityTests/DatabaseSetup.cs
+++ ContosoUniversity/ContosoUniversityTests/DatabaseSetup.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using System.Linq;
+using System.Data.SqlClient;
 
 namespace ContosoUniversityTests
 {
@@ -28,8 +29,7 @@ namespace ContosoUniversityTests
             Assert.AreEqual(TaskStatus.RanToCompletion, task.Status,
                 "department did not make, task did not complete correctly");
             
-            // crap!
-            //DoesDepartmentExist(true);
+            DoesDepartmentExist(true);
 
             objects.SetDepartmentID(objects.department.DepartmentID);
 
@@ -47,6 +47,66 @@ namespace ContosoUniversityTests
         protected void DatabaseReset()
         {
             db.Database.Delete();
+        }
+
+        protected int InstructorCountByCourse(int courseID)
+        {
+            string query = "select count(InstructorID) from CourseInstructor where CourseID = @CourseID";
+            return db.Database.SqlQuery<int>(query, new SqlParameter("@CourseID", courseID)).SingleOrDefault();
+        }
+
+        protected void HowManyStudents(int num)
+        {
+            Assert.AreEqual<int>(num, db.Students.Where(s => s.EnrollmentDate == objects.studentEnrollmentDate).Count(),
+                "incorrect number of students");
+        }
+
+        protected void HowManyOfficeAssignments(int num)
+        {
+            Assert.AreEqual<int>(num, db.OfficeAssignments.Where(o => o.Location == objects.officeLocation).Count(),
+                "not enough office locations deleted");
+        }
+
+        protected void HowManyCourseInstructorEntries(int num)
+        {
+            for (int i = 0; i < objects.NumberOfDerivativeObjects; i++)
+            {
+                Assert.AreEqual<int>(num, InstructorCountByCourse(objects.Courses[i].CourseID),
+                    "incorrect number of instructors for course");
+            }
+        }
+
+        protected void HowManyInstructors(int num)
+        {
+            Assert.AreEqual<int>(num, db.Instructors.Where(i => i.DepartmentID == objects.department.DepartmentID).Count(),
+                "incorrect number of instructors");
+        }
+
+        protected void HowManyCourses(int num)
+        {
+            Assert.AreEqual<int>(num, db.Courses.Where(i => i.DepartmentID == objects.department.DepartmentID).Count(),
+                "incorrect number of courses");
+        }
+
+        protected void DoEnrollmentsExist(bool yesOrNo)
+        {
+            int[] studentIds = objects.Students.Select(o => o.ID).ToArray();
+            if (yesOrNo)
+                Assert.IsTrue(db.Enrollments.Where(s => studentIds.Contains(s.StudentID)).Count() != 0/*.SingleOrDefault()*/,
+                    "incorrect number of enrollments");
+            else
+                Assert.IsTrue(db.Enrollments.Where(s => studentIds.Contains(s.StudentID)).Count() == 0/*.SingleOrDefault()*/,
+                    "incorrect number of enrollments");
+        }
+
+        protected void DoesDepartmentExist(bool yesOrNo)
+        {
+            if (yesOrNo)
+                Assert.IsNotNull(db.Departments.Where(d => d.Name == objects.department.Name).SingleOrDefault(),
+                    "department exists");
+            else
+                Assert.IsNull(db.Departments.Where(d => d.Name == objects.department.Name).SingleOrDefault(),
+                    "department does not exist");
         }
 
         //[TestCleanup()]
