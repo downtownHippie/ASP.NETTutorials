63a14a6baaa6b01ee484627c092eb9d16f2501ca
Joe Reid <jreid@vnet.net>
2014-07-21 10:10:33 -0500

    now that grade is not an enum webservice can utilize main project

--- ContosoUniversity/ContosoUniversityWebService/WebService/ContosoUniversityWebService.svc.cs
+++ ContosoUniversity/ContosoUniversityWebService/WebService/ContosoUniversityWebService.svc.cs
@@ -3,7 +3,7 @@
 //     Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>
 //------------------------------------------------------------------------------
-using ContosoUniversityWebService.DAL;
+using ContosoUniversity.DAL;
 using System.Data.Services;
 using System.Data.Services.Common;
 using System.Data.Services.Providers;
