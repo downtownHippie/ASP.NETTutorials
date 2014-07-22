46530c0b3ddb33d0a362f932b7db71045a64a54f
Joe Reid <jreid@vnet.net>
2014-07-22 17:16:45 -0500

    added a few returns

--- ContosoUniversity/ContosoUniversity/Controllers/DepartmentController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/DepartmentController.cs
@@ -18,7 +18,8 @@ namespace ContosoUniversity.Controllers
         // GET: Department
         public async Task<ActionResult> Index()
         {
-            var departments = db.Departments.Include(d => d.Administrator)
+            var departments = db.Departments
+                .Include(d => d.Administrator)
                 .OrderBy(d => d.Name);
             return View(await departments.ToListAsync());
         }
@@ -133,8 +134,11 @@ namespace ContosoUniversity.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            Department department = await db.Departments.Include("Instructors.OfficeAssignment")
-                .Include("Courses.Instructors").Where(o => o.DepartmentID == id).SingleAsync();
+            Department department = await db.Departments
+                .Include("Instructors.OfficeAssignment")
+                .Include("Courses.Instructors")
+                .Where(o => o.DepartmentID == id)
+                .SingleAsync();
 
             db.Departments.Remove(department);
             await db.SaveChangesAsync();
